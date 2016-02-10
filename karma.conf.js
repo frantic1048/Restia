69250ac255481a40d70dee890bf77f817ceae8bc
frantic1048 <archer@frantic1048.com>
2016-02-11 05:04:44 +0800

    v0.0.4: meta reducers

--- karma.conf.js
+++ karma.conf.js
@@ -66,7 +66,6 @@ module.exports = function(config) {
     },
 
     reporters: ['spec', 'coverage'],
-
     coverageReporter: {
       type: 'text',
     },
--- test/spec/restiaSpec.js
+++ test/spec/restiaSpec.js
@@ -4,64 +4,122 @@ import { createAction } from 'redux-actions';
 
 import types from '../../lib/constants/ActionTypes';
 import actions from '../../lib/actions';
+import reducer from '../../lib/reducers';
 
 const middlewares = [ thunk ];
 const mockStore = configureMockStore(middlewares);
 
-const siteMeta = {
-  siteName: 'Restia Blog',
-  authorName: 'Restia',
-  URL: 'https://github.com/frantic1048/Restia',
-  links: {
-    npmjs: 'https://www.npmjs.com/',
-    iojs: 'https://iojs.org/',
-  },
-  articleBase: './',
-};
-
 describe('site meta data', () => {
-  beforeEach(() => {
-    sinon.stub(window, 'fetch');
+  const siteMeta = {
+    siteName: 'Restia Blog',
+    authorName: 'Restia',
+    URL: 'https://github.com/frantic1048/Restia',
+    links: {
+      npmjs: 'https://www.npmjs.com/',
+      iojs: 'https://iojs.org/',
+    },
+    articleBase: './',
+  };
+
+  const siteMetaURL = '/restia_meta.json';
+
+  const initialMetaState = {
+    isFetching: false,
+    fetchingError: null,
+    siteName: '',
+    authorName: '',
+    URL: '',
+    links: {},
+    articleBase: './',
+  };
+
+  const actRequest = createAction(types.FETCH_META_REQUEST)();
+
+  it('should have initial meta state', () => {
+    expect(reducer(undefined, {}).meta)
+      .toEqual(initialMetaState);
   });
 
-  afterEach(() => {
-    window.fetch.restore();
-  });
-
-  it('should FETCH_META_SUCCESS with meta data when fetcing succeed', (done) => {
+  describe('fetching success', () => {
     const res = new window.Response(JSON.stringify(siteMeta), {
       status: 200,
       headers: {
         'Content-type': 'application/json',
       },
     });
+    const actSuccess = createAction(types.FETCH_META_SUCCESS)(siteMeta);
 
-    window.fetch.returns(Promise.resolve(res));
+    beforeAll(() => {
+      sinon.stub(window, 'fetch');
+      window.fetch.withArgs(siteMetaURL).returns(Promise.resolve(res));
+    });
 
-    const expectedActions = [
-      act => expect(act).toEqual(createAction(types.FETCH_META_REQUEST)()),
-      act => expect(act).toEqual(createAction(types.FETCH_META_SUCCESS)(siteMeta)),
-    ];
+    afterAll(() => {
+      window.fetch.restore();
+    });
 
-    const store = mockStore({ meta: {} }, expectedActions, done);
-    store.dispatch(actions.fetchMeta());
+    it('should FETCH_META_SUCCESS', (done) => {
+      const expectedActions = [
+        act => expect(act).toEqual(actRequest),
+        act => expect(act).toEqual(actSuccess),
+      ];
+
+      const store = mockStore({ meta: {} }, expectedActions, done);
+      store.dispatch(actions.fetchMeta());
+    });
+
+    it('should return fetching state', () => {
+      expect(reducer(undefined, actRequest).meta)
+        .toEqual({
+          ...initialMetaState,
+          isFetching: true,
+          fetchingError: null,
+        });
+    });
+
+    it('should return state with meta', () => {
+      expect(reducer(undefined, actSuccess).meta)
+        .toEqual({
+          ...initialMetaState,
+          ...siteMeta,
+          isFetching: false,
+          fetchingError: null,
+        });
+    });
   });
 
-  it('should FETCH_META_FAILURE when fetching fails', (done) => {
+  describe('fecthing failure', () => {
     const res = new window.Response('', {
       status: 404,
     });
+    const actFailure = createAction(types.FETCH_META_FAILURE)(res);
 
-    window.fetch.returns(Promise.reject(res));
+    beforeAll(() => {
+      sinon.stub(window, 'fetch');
+      window.fetch.returns(Promise.reject(res));
+    });
 
-    const expectedActions = [
-      act => expect(act).toEqual(createAction(types.FETCH_META_REQUEST)()),
-      act => expect(act).toEqual(createAction(types.FETCH_META_FAILURE)(res)),
-    ];
+    afterAll(() => {
+      window.fetch.restore();
+    });
 
-    const store = mockStore({ meta: {} }, expectedActions, done);
-    store.dispatch(actions.fetchMeta());
+    it('should FETCH_META_FAILURE', (done) => {
+      const expectedActions = [
+        act => expect(act).toEqual(actRequest),
+        act => expect(act).toEqual(actFailure),
+      ];
+
+      const store = mockStore({ meta: {} }, expectedActions, done);
+      store.dispatch(actions.fetchMeta());
+    });
+
+    it('should return state with error info', () => {
+      expect(reducer(undefined, actFailure).meta)
+        .toEqual({
+          ...initialMetaState,
+          isFetching: false,
+          fetchingError: res,
+        });
+    });
   });
-
-  // TODO: also testing on reducers
 });
