b8684fa0582eb3be301818b8d79d725f5df68c62
frantic1048 <archer@frantic1048.com>
2016-02-14 04:01:05 +0800

    posts index fetching stuff

---
+++ test/asserts/restia_meta.js
@@ -0,0 +1,10 @@
+export default {
+  siteName: 'Restia Blog',
+  authorName: 'Restia',
+  URL: 'https://github.com/frantic1048/Restia',
+  links: {
+    npmjs: 'https://www.npmjs.com/',
+    iojs: 'https://iojs.org/',
+  },
+  articleBase: './',
+};
