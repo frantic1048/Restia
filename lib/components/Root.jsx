21d2345e837a62c17efd08589d80dec52f305dcb
frantic1048 <archer@frantic1048.com>
2016-03-31 22:50:03 +0800

    v0.0.20: Root component ok~

--- lib/components/Root.jsx
+++ lib/components/Root.jsx
@@ -1,15 +1,14 @@
 import React              from 'react';
 import { Provider }       from 'react-redux';
-import { Router, browserHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 
 import store from '../store';
 
-const history = syncHistoryWithStore(browserHistory, store, {
+const history = syncHistoryWithStore(hashHistory, store, {
   selectLocationState: (state) => state.get('routing'),
 });
 
-// TODO: test me!
 export default (content) => ( // eslint-disable-line no-extra-parens
   <Provider store={store}>
     <Router history={history}>
