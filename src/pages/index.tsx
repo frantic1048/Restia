b56051eef26941e51fa3b00e53aa0324bf88398e
frantic1048 <i@frantic1048.com>
2019-08-01 01:51:22 +0800

    refactor: remove redundant assignment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,15 +4,6 @@ import { oc } from 'ts-optchain'
 import { GatsbyComponent } from 'util/types'
 import { SiteMetadataQuery } from '../../types/graphql-types'
 
-const Page: GatsbyComponent<SiteMetadataQuery> = z => {
-    const data = z.data
-    return (
-        <div>
-            Welcome to <b>{oc(data).site.siteMetadata.title('')}</b>
-        </div>
-    )
-}
-export default Page
 export const query = graphql`
     query siteMetadata {
         site {
@@ -22,3 +13,12 @@ export const query = graphql`
         }
     }
 `
+
+const Page: GatsbyComponent<SiteMetadataQuery> = ({ data }) => {
+    return (
+        <div>
+            Welcome to <b>{oc(data).site.siteMetadata.title('')}</b>
+        </div>
+    )
+}
+export default Page
