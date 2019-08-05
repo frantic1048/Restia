48932343551adb9019a19f52c6d85f416742ecfd
frantic1048 <i@frantic1048.com>
2019-08-06 00:14:26 +0800

    style(gatsby): format gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,8 +10,8 @@ module.exports = {
             resolve: 'gatsby-source-filesystem',
             options: {
                 name: 'posts',
-                path: `${__dirname}/src/posts`
-            }
+                path: `${__dirname}/src/posts`,
+            },
         },
         'gatsby-transformer-remark',
     ],
