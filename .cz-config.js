c86c5930b8c12a844a762fc6d07f40d98d957564
frantic1048 <i@frantic1048.com>
2021-05-25 19:42:00 +0800

    style: make prettier happy

--- .cz-config.js
+++ .cz-config.js
@@ -9,14 +9,12 @@ module.exports = {
         },
         {
             value: 'style',
-            name:
-                'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)',
+            name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)',
         },
         { value: 'test', name: 'test:     Adding missing tests' },
         {
             value: 'chore',
-            name:
-                'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation',
+            name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation',
         },
     ],
 
